12d59a5e14353f18a5c897f3588d6b620c66b0c6
Irena Jurasek <irenaskydive@gmail.com>
2020-01-04 14:02:53 +0100

    add setProperty method on root

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const inputs = document.querySelectorAll('.controls input');
 // dataset property provides access to all custom data attibutes set on an element
 function handleUpdate() {
     const suffix = this.dataset.sizing || ''; // empty string prevents appending of undefined for the color input
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    // document.documentElement returns the Element that is the root element of the document
+    // setProperty method sets a new value for a property on a CSS style declaration object
 }
 
 // listen for a change and mousemove event on each input 
